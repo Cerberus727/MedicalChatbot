8ce223e152120ec385ba0735515984479d1c663a
Cerberus727 <ashutosh263646@gmail.com>
2025-08-28 00:40:19 +0530

    Added new files and updated existing ones

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Ashutosh",
+    author_email="ashutosh263646@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
